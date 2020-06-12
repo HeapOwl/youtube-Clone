0be24849e7edc7098991ba094f87067d4fd1c5c0
Prateek <prateekrai7677@gmail.com>
2020-06-12 16:19:53 +0530

    Updates

---
+++ precache-manifest.fe6cb63266765a10d3f055b006daae84.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38698ba265e0d3a9e2c8eb75384446a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518c85b1577bffc176d6",
+    "url": "/static/js/2.2586805c.chunk.js"
+  },
+  {
+    "revision": "0593f245cb727246fbc9",
+    "url": "/static/js/main.5e1d7eee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
