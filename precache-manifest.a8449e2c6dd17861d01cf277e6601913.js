1e87bfd3d3d3175fc9f9d725aec6642a7dc3125f
Prateek <prateekrai7677@gmail.com>
2020-06-12 16:28:00 +0530

    Updates

---
+++ precache-manifest.a8449e2c6dd17861d01cf277e6601913.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd7d83bd71b2f3f34a58b6df0d494b9",
+    "url": "/youtube-Clone/index.html"
+  },
+  {
+    "revision": "2186302aedd0a9f98017",
+    "url": "/youtube-Clone/static/js/2.326c9171.chunk.js"
+  },
+  {
+    "revision": "0593f245cb727246fbc9",
+    "url": "/youtube-Clone/static/js/main.5e1d7eee.chunk.js"
+  },
+  {
+    "revision": "c0bd6ad129ad9d96bb6b",
+    "url": "/youtube-Clone/static/js/runtime~main.05533d43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe6cb63266765a10d3f055b006daae84.js"
+  "/youtube-Clone/precache-manifest.a8449e2c6dd17861d01cf277e6601913.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtube-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
